146eb570f37f7aafa333601ebcbfd93fb3920d21
Pavel <sozonovps@gmail.com>
2023-01-28 18:01:18 +0800

    Добавьте файлы проекта.

---
+++ task1/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal abstract class Anymal
+    {
+        public abstract string Name { get; set; }
+        public Anymal(string name)
+        {
+           Name = name;
+        }
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+
+    internal class Cat : Anymal
+    {
+        private string name;
+        public override string Name 
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Cat(string name)
+            :base(name)
+        {
+
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+    internal class Dog : Anymal
+    {
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name)
+    : base(name)
+        {
+
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+
+}
+
---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Мурена");
+            Dog dog = new Dog("Шарик");
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
